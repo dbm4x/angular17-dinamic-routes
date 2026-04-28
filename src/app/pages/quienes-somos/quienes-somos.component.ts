0c22693b8b7f90e61c98234b1cdeee5c7eff95b8
dbm4x <roc666@protonmail.com>
2026-04-28 19:43:18 -0300

    test

--- src/app/pages/quienes-somos/quienes-somos.component.ts
+++ src/app/pages/quienes-somos/quienes-somos.component.ts
@@ -5,10 +5,18 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-quienes-somos',
   standalone: true,
-  imports: [RouterLink],
+ imports: [CommonModule, RouterLink],
   templateUrl: './quienes-somos.component.html',
   styleUrl: './quienes-somos.component.css'
 })
 export class QuienesSomosComponent {
+
+  integrantes = [
+    { id: 1, nombre: 'Lanfranco Darel Caballero', rol: 'Desarrollador Web' },
+    { id: 2, nombre: 'Luis Gerardo Catalas', rol: 'Desarrollador Web' },
+    { id: 3, nombre: 'Lautaro Ancillotti', rol: 'Diseñador' },
+    { id: 4, nombre: 'Gerardo Lauro Romero', rol: 'Diseñador' },
+    { id: 5, nombre: 'Claudio Nicolas Audicio', rol: 'Diseñador' }
+  ];
 
 }
